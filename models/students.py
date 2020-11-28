ca84116863077bde43f67fe2bf62ad54a820348c
Somanath Chowdary <somanath.chowdary@ymail.com>
2020-11-28 14:53:06 +0530

    Add Database Model for Students table

---
+++ models/students.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Date, DateTime
+
+
+class Students(Base):
+    __tablename__ = "students"
+
+    # Feilds for Students Table
+    roll_number = Column('roll_number', String(10), primary_key=True)
+    name = Column('name', String(32), nullable=False)
+    dob = Column('dob', Date, nullable=False)
+    registration_date = Column('registration_date', DateTime, nullable=False)
