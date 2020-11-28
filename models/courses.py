1a871a1de0e216b56abceeae2139df30083a56bb
Somanath Chowdary <somanath.chowdary@ymail.com>
2020-11-28 15:04:43 +0530

    Add Database Model for Courses Table

---
+++ models/courses.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Date, DateTime, Integer
+
+
+class Courses(Base):
+    __tablename__ = "courses"
+
+    # Feilds for Courses Table
+    course_id = Column('course_id', String(10), primary_key=True)
+    name = Column('name', String(64), nullable=False)
+    duration_in_hours = Column('duration_in_hours', Integer, nullable=False)
+    fee = Column('fee', Integer, nullable=False)
+    date_of_creation = Column('date_of_creation', DateTime, nullable=False)
