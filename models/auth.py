cae6c6e8e006ba574e0c1d13c1ee46657e9c17f2
Somanath Chowdary <somanath.chowdary@ymail.com>
2020-11-28 14:58:39 +0530

    Add Database model for auth table

---
+++ models/auth.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String, Date, DateTime
+
+
+class Auth(Base):
+    __tablename__ = "auth"
+
+    # Feilds for Auth table
+    username = Column('username', String(10), primary_key=True)
+    password = Column('password', String, nullable=False)
+    last_login = Column('last_login', DateTime, nullable=False)
