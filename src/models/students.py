cdbb3299d866eea211976cc9534d75b0d63413d2
Somanath Chowdary <somanath.chowdary@ymail.com>
2020-11-29 02:11:56 +0530

    Remove instance method for student model

--- src/models/students.py
+++ src/models/students.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, String, Date, DateTime
-from sqlalchemy.orm import Session
 from .base import Base
 
 class Students(Base):
@@ -10,8 +9,3 @@ class Students(Base):
     name = Column('name', String(32), nullable=False)
     dob = Column('dob', Date, nullable=False)
     registration_date = Column('registration_date', DateTime, nullable=False)
-
-    def insert(self):
-        session = Session.object_session(self)
-        session.add(self)
-        session.commit()
